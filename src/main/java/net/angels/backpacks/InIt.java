d86429c55e2af851062f7c176f5266b7a8011e82
Brandocks <37669059+Brandocks@users.noreply.github.com>
2020-12-22 14:21:49 -0500

    Foundation

--- src/main/java/net/angels/backpacks/InIt.java
+++ src/main/java/net/angels/backpacks/InIt.java
@@ -1,14 +1,16 @@
-package net.fabricmc.example;
+package net.angels.backpacks;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class InIt implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+		System.out.println("Thank you for choosing Angel's Backpacks! Now initializing...");
 	}
+
+	public static void
 }
